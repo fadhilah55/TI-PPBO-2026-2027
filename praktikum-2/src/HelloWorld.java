13251480c6fcfeb38a2c8968dd6407429a7cece7
Fadhilah <fadhilahh0527@gmail.com>
2026-09-11 22:29:09 +0700

    A.2. Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,4 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo, Fadhilah");
-}
+
+        System.out.print("Nama: ");
+        System.out.print("Fadhilah");
+        System.out.println();
+        System.out.println("Ini baris baru");
+
+    }
